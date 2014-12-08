b8ed7d0e22d11f4fd097a5aed93bbf3514903f69
anders froyhaug <anders.froyhaug@i-nor.com>
2014-12-08 17:36:02 +0100

    init

---
+++ ArduinoPanTiltServo.h
@@ -0,0 +1,14 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef ArduinoPanTiltServo_H
+#define ArduinoPanTiltServo_H
+#include "Arduino.h"
+#include <Servo.h>
+
+
+
+#endif
